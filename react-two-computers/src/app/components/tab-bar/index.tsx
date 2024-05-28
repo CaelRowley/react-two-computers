6cb7b492852926c1b96b8d12f37251f09bb1612c
Cael Rowley <drdeus@pm.me>
2024-05-28 09:34:06 +0200

    Add fake slow component on tab

--- react-two-computers/src/app/components/tab-bar/index.tsx
+++ react-two-computers/src/app/components/tab-bar/index.tsx
@@ -1,7 +1,22 @@
+import { Suspense } from "react";
 import { TabBarClient } from "./client";
 
 export function TabBar() {
   console.log("Render tab bar")
-  
-  return <TabBarClient a={<div>A</div>} b={<div>B</div>} c={<div>C</div>} />;
+
+  return <TabBarClient
+    a={<div>A</div>}
+    b={
+      <Suspense fallback={<div>Loading...</div>}>
+        <SlowComponent />
+      </Suspense>
+    }
+    c={<div>C</div>}
+  />;
+}
+
+async function SlowComponent() {
+  await new Promise((resolve) => setTimeout(resolve, 5000))
+
+  return <div>B</div>
 }
